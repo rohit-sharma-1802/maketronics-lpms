a03129a45d99d05021a1bf7915190914252aaefb
Rohit sharma <rohitsharmatech9@gmail.com>
2024-10-24 21:12:35 +0530

    changes

--- maketronics-backend/index.js
+++ maketronics-backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // React app origin
+    origin: 'https://maketronics-lpms-bxmh.vercel.app/', // React app origin
     credentials: true, // Allow credentials (cookies) to be sent
 };
 
