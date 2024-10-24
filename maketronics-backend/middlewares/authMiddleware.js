96c8ff15ecd516febd827f336d20a3f663b30978
Rohit sharma <rohitsharmatech9@gmail.com>
2024-10-25 00:06:37 +0530

    fire

--- maketronics-backend/middlewares/authMiddleware.js
+++ maketronics-backend/middlewares/authMiddleware.js
@@ -4,9 +4,9 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.token; // Get token from cookies
   console.log(token);
 
-  if (!token) {
-    return res.status(401).json({ error: 'Access denied. No token provided.' });
-  }
+  // if (!token) {
+  //   return res.status(401).json({ error: 'Access denied. No token provided.' });
+  // }
 
   try {
     const decoded = jwt.verify(token, process.env.WEBTOKEN_KEY); // Verify token
